9e5029619516b246ae6dfd7523ee00b06a05fcb6
shahed chowdhury <shahed.33cse@gmail.com>
2020-09-21 17:12:34 +0600

    pop up form

--- js/app.js
+++ js/app.js
@@ -6,3 +6,21 @@ navItem.forEach((btn) => {
     this.classList.add('active');
   });
 });
+
+//////pop up form
+const popUpBtn = document.querySelectorAll('.pop-up-btn');
+const form = document.querySelector('form');
+const closeBtn = document.querySelector('.close-btn');
+
+popUpBtn.forEach((btn) => {
+  btn.addEventListener('click', function () {
+    form.style.display = 'block';
+  });
+});
+
+// popUpBtn.addEventListener('click', () => {
+//   form.style.display = 'block';
+// });
+closeBtn.addEventListener('click', () => {
+  form.style.display = 'none';
+});
